f3dd037a846ef8941228de3fb8e087a50b7a791f
inmokang <kingkongapps@gmail.com>
2025-04-20 23:50:20 +0900

    first commit - authorization test...

---
+++ src/main/java/com/example/keycloak/site4_role_test/Site4RoleTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.keycloak.site4_role_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Site4RoleTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Site4RoleTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/keycloak/site4_role_test/Site4RoleTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keycloak.site4_role_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Site4RoleTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
